6a972c3d3ef394794f258fe999ca279c2287fec3
Wk-P <72795540+Wk-P@users.noreply.github.com>
2022-05-19 23:03:44 +0900

    Create Selection_Sort.cpp

---
+++ Selection_Sort.cpp
@@ -0,0 +1,14 @@
+void Selection_Sort(int* arr, int size) {
+	// find minimum 
+	for (int i = 0; i < size - 1; i++) {
+		int min = i;
+		for (int j = i + 1; j < size; j++) {
+			if (arr[j] < arr[min]) {
+				min = j;
+			}
+		}
+		int temp = arr[i];
+		arr[i] = arr[min];
+		arr[min] = temp;
+	}
+}
